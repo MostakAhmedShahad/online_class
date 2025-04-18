f4b466e382d6f7034d834e9fe7239af8a2711fae
Mostak <mostakahmedshahad74@gmail.com>
2025-04-19 01:13:09 +0600

    responsive_design

--- lib/pages/welcome/welcome.dart
+++ lib/pages/welcome/welcome.dart
@@ -16,62 +16,78 @@ class _WelcomeState extends State<Welcome> {
       color: Colors.white,
       child: Scaffold(
         body: Container(
-          // width: 375.w,
           child: Stack(
             children: [
               PageView(
                 children: [
-                  Column(
-                    children: [
-                      SizedBox(
-                        height: 360,
-                        width: 360,
-                        child: Text('image one'),
-                      ),
-                      Container(
-                        child: Text(
-                          'First see learning',
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 24,
-                              fontWeight: FontWeight.normal),
-                        ),
-                      ),
-                      Container(
-                        padding: EdgeInsets.all(12),
-                        child: Text(
-                          'Forget about a for of paper all knowledge in learning',
-                          style: TextStyle(
-                              color: Colors.black.withOpacity(0.5),
-                              fontSize: 14,
-                              fontWeight: FontWeight.normal),
-                        ),
-                      ),
-                      Container(
-                        padding: EdgeInsets.all(20),
-                        height: 50,
-                        width: 325,
-                        decoration: BoxDecoration(
-                            color: Colors.blue,
-                            borderRadius: BorderRadius.circular(12),
-                            boxShadow: [
-                              BoxShadow(
+                  LayoutBuilder(
+                    builder: (context, constraints) {
+                      // Get the screen width and height
+                      final screenWidth = constraints.maxWidth;
+                      final screenHeight = constraints.maxHeight;
+
+                      return Column(
+                        children: [
+                          SizedBox(
+                            height: screenHeight *
+                                0.4, // e.g., 40% of screen height
+                            width:
+                                screenWidth * 0.8, // e.g., 80% of screen width
+                            child: Text('image one'),
+                          ),
+                          Container(
+                            child: Text(
+                              'First see learning',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize:
+                                    screenWidth * 0.06, // responsive font size
+                                fontWeight: FontWeight.normal,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.all(screenWidth * 0.03),
+                            child: Text(
+                              'Forget about a for of paper all knowledge in learning',
+                              style: TextStyle(
+                                color: Colors.black.withOpacity(0.5),
+                                fontSize:
+                                    screenWidth * 0.035, // responsive font size
+                                fontWeight: FontWeight.normal,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.all(screenWidth * 0.05),
+                            height: screenHeight * 0.06,
+                            width: screenWidth * 0.85,
+                            decoration: BoxDecoration(
+                              color: Colors.blue,
+                              borderRadius: BorderRadius.circular(12),
+                              boxShadow: [
+                                BoxShadow(
                                   color: Colors.grey.withOpacity(.5),
                                   spreadRadius: 1,
                                   blurRadius: 2,
-                                  offset: Offset(0, 1))
-                            ]),
-                        child: Center(
-                          child: Text(
-                            'Next',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 14,
-                                fontWeight: FontWeight.normal),
-                          ),
-                        ),
-                      )
-                    ],
+                                  offset: Offset(0, 1),
+                                )
+                              ],
+                            ),
+                            child: Center(
+                              child: Text(
+                                'Next',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: screenWidth * 0.04, // responsive
+                                  fontWeight: FontWeight.normal,
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      );
+                    },
                   )
                 ],
               )
