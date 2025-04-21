7ceb0d23bf8ea75e84b70bd829cad98e22195a6f
Mostak <mostakahmedshahad74@gmail.com>
2025-04-22 00:32:38 +0600

    welcome_bloc

---
+++ lib/pages/welcome/bloc/welcome_bloc.dart
@@ -0,0 +1,11 @@
+ import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:online_class/pages/welcome/bloc/welcome_event.dart';
+import 'package:online_class/pages/welcome/bloc/welcome_state.dart';
+
+class WelcomBloc extends Bloc<WelcomeEvent, WelcomeState> {
+  WelcomBloc() : super(WelcomeState()) {
+    on<WelcomeEvent>((event, emit) {
+      
+    });
+  }
+}  
