4fc346887dee7d96f27c14d312c3699d1d330347
Mostak <mostakahmedshahad74@gmail.com>
2025-04-26 23:26:22 +0600

    AppBar_widget

---
+++ lib/pages/sign_in/widgets/sign_in_widget.dart
@@ -0,0 +1,20 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+AppBar buildAppBar(){
+
+  return  AppBar(
+          bottom: PreferredSize(preferredSize: const Size.fromHeight(1), child: Container(
+            color: Colors.grey.withOpacity(.5),
+            height: 1,
+          )),
+          title: Center(
+            child: Text('Log In',style: TextStyle(
+              color: Colors.black,
+              fontSize: 22,
+              fontWeight: FontWeight.normal,
+            ),),
+          ),
+        );
+}
