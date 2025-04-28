62b1ae3b53c7489d4b633e3cd469074367014bc0
Mostak <mostakahmedshahad74@gmail.com>
2025-04-29 02:43:32 +0600

    bloc_complete

--- lib/pages/sign_in/bloc/sign_in_bloc.dart
+++ lib/pages/sign_in/bloc/sign_in_bloc.dart
@@ -1,11 +1,18 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:online_class/pages/sign_in/bloc/sign_in_event.dart';
 import 'package:online_class/pages/sign_in/bloc/sign_in_state.dart';
 
 class SignInBloc extends Bloc<SignInEvent, SignInState> {
   SignInBloc() : super(SignInState()) {
-    on<SignInEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+    on<EmailEvent>(_emailEvent);
+    on<PasswordEvent>(_passwordEvent);
+  }
+  void _emailEvent(EmailEvent event, Emitter<SignInState> emit) {
+    emit(state.copyWith(email: event.email));
+  }
+
+  void _passwordEvent(PasswordEvent event, Emitter<SignInState> emit) {
+    emit(state.copyWith(password: event.password));
   }
 }
