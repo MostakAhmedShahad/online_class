7f8569378742a5f0d7dd258b1b6183e45c43ca49
Mostak <mostakahmedshahad74@gmail.com>
2025-04-29 02:32:20 +0600

    sign_in_bloc

--- lib/pages/sign_in/bloc/sign_in_bloc.dart
+++ lib/pages/sign_in/bloc/sign_in_bloc.dart
@@ -1,3 +1,11 @@
-class SignInBloc {
-  
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:online_class/pages/sign_in/bloc/sign_in_event.dart';
+import 'package:online_class/pages/sign_in/bloc/sign_in_state.dart';
+
+class SignInBloc extends Bloc<SignInEvent, SignInState> {
+  SignInBloc() : super(SignInState()) {
+    on<SignInEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
 }
