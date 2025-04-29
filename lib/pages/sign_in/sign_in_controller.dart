7434e908659c90b885d666bbf8610039095ee0cb
Mostak <mostakahmedshahad74@gmail.com>
2025-04-30 00:57:01 +0600

    emailVerified

--- lib/pages/sign_in/sign_in_controller.dart
+++ lib/pages/sign_in/sign_in_controller.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:online_class/main.dart';
@@ -6,31 +7,27 @@ import 'package:online_class/pages/sign_in/bloc/sign_in_bloc.dart';
 class SignInController {
   final BuildContext context;
   const SignInController({required this.context});
-  void hanldeSingIn(String type){
-    try{
-      if(type=="email"){
-        final state= context.read<SignInBloc>().state;
-        String emailAddress=state.email;
-        String password=state.password;
-        if(emailAddress.isEmpty){
+  Future<void> hanldeSingIn(String type) async {
+    try {
+      if (type == "email") {
+        final state = context.read<SignInBloc>().state;
+        String emailAddress = state.email;
+        String password = state.password;
+        if (emailAddress.isEmpty) {}
+        if (password.isEmpty) {
+          final credential = await FirebaseAuth.instance
+              .signInWithEmailAndPassword(
+                  email: emailAddress, password: password);
+                  if(credential.user==null){
 
-        }
-        if(password.isEmpty){
-
-        }
-        try{
-          
+                  }
+                  if(!credential.user!.emailVerified){
+                    
+                  }
         }
 
-        catch(e){
-
-        }
+        try {} catch (e) {}
       }
-    }
-    catch(e){
-      
-    }
-
-
+    } catch (e) {}
   }
 }
