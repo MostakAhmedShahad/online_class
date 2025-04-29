645eccabcdc32418550a3fe00502067d02da8ef3
Mostak <mostakahmedshahad74@gmail.com>
2025-04-29 23:35:03 +0600

    try_catch

--- lib/pages/sign_in/sign_in_controller.dart
+++ lib/pages/sign_in/sign_in_controller.dart
@@ -1,6 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:online_class/main.dart';
+import 'package:online_class/pages/sign_in/bloc/sign_in_bloc.dart';
 
 class SignInController {
   final BuildContext context;
-  const SignInController(this.context);
+  const SignInController({required this.context});
+  void hanldeSingIn(String type){
+    try{
+      if(type=="email"){
+        final state= context.read<SignInBloc>().state;
+        String emailAddress=state.email;
+        String password=state.password;
+        if(emailAddress.isEmpty){
+
+        }
+        if(password.isEmpty){
+          
+        }
+      }
+    }
+    catch(e){
+      
+    }
+
+
+  }
 }
